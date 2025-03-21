641b18a36962189e998e2000584da02cb02a11a1
Luiz <luizkdz@github.com>
2025-03-21 19:29:03 -0300

    Mudando Header

--- src/PaginaInicial/index.js
+++ src/PaginaInicial/index.js
@@ -6,69 +6,10 @@ import './styles.css';
 import Banner from '../componentes/banner/index.js';
 import SessaoProdutos from "../componentes/secao-produtos/index.js";
 function PaginaInicial() {
-  const [nome, setNome] = useState("");
+  
   const navigate = useNavigate();
 
-  const refreshAccessToken = async () => {
-    try{
-      const resposta = await axios.post("http://localhost:5000/renovarsessao", {}, { withCredentials: true });
-      const token = resposta.data.token;
-      return token;
-    }
-    catch(err){
-      return null;
-    }
-  }
-  const pickName = async () => {
-    try {
-      const resposta = await axios.get("http://localhost:5000/paginainicial", { withCredentials: true });
-      if (resposta.data) {
-        setNome(resposta.data.nome);
-      }
-      console.log("Nome recebido:", resposta.data);
-    } catch (error) {     
-      if ((error.response && error.response.status === 401) || (error.response && error.response.status === 403)){
-        const novoTokenAcesso = await refreshAccessToken();
-
-        if (novoTokenAcesso) {
-          try {
-            const novaResposta = await axios.get("http://localhost:5000/paginainicial", {withCredentials:true});
-          
-            if (novaResposta.data) {
-              setNome(novaResposta.data.nome);
-            }
-          } catch (novoErro) {
-            console.error("Erro ao buscar nome após renovação do token:", novoErro);
-            alert("Não foi possível renovar o token.");
-          }
-        } else {  
-          console.warn("Token não pôde ser renovado. Usuário precisa fazer login novamente.");
-        }
-      }
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await pickName();
-    };
-    
   
-    fetchData();
-  }, []);
-
-  const handleLogout = async () => {
-    try{
-        const resposta = await axios.post("http://localhost:5000/logout", {}, {withCredentials: true});
-        
-        alert(resposta.data.message);
-    }
-
-    catch(error){
-        alert("Erro ao fazer logout");
-    }
-    navigate('/');
-  }
 
   
   return (
@@ -77,8 +18,7 @@ function PaginaInicial() {
       <Banner/>
       <SessaoProdutos/>
       <div className= "container-data">
-      <h1>Bem-vindo, {nome} </h1>
-      <button onClick={handleLogout}>Logout</button>
+      
     </div>
     </div>
   );
--- src/componentes/header/index.js
+++ src/componentes/header/index.js
@@ -1,6 +1,88 @@
 import './styles.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import './styles.css';
+import { useNavigate } from 'react-router-dom';
+
+
+
 
 function Header({props}) {
+    const [nome, setNome] = useState("");
+    const navigate = useNavigate();
+    const [menuAberto, setMenuAberto] = useState(false);
+    const toggleMenu = () => {
+        setMenuAberto(!menuAberto);
+    };
+
+    const refreshAccessToken = async () => {
+        try{
+          const resposta = await axios.post("http://localhost:5000/renovarsessao", {}, { withCredentials: true });
+          const token = resposta.data.token;
+          return token;
+        }
+        catch(err){
+          return null;
+        }
+      }
+    const pickName = async () => {
+        try {
+          const resposta = await axios.get("http://localhost:5000/paginainicial", { withCredentials: true });
+          if (resposta.data) {
+            setNome(resposta.data.nome);
+          }
+          console.log("Nome recebido:", resposta.data);
+        } catch (error) {     
+          if ((error.response && error.response.status === 401) || (error.response && error.response.status === 403)){
+            const novoTokenAcesso = await refreshAccessToken();
+    
+            if (novoTokenAcesso) {
+              try {
+                const novaResposta = await axios.get("http://localhost:5000/paginainicial", {withCredentials:true});
+              
+                if (novaResposta.data) {
+                  setNome(novaResposta.data.nome);
+                }
+              } catch (novoErro) {
+                console.error("Erro ao buscar nome após renovação do token:", novoErro);
+                alert("Não foi possível renovar o token.");
+              }
+            } else {  
+              console.warn("Token não pôde ser renovado. Usuário precisa fazer login novamente.");
+            }
+          }
+        }
+      };
+      useEffect(() => {
+        const fetchData = async () => {
+          await pickName();
+        };
+        
+      
+        fetchData();
+      }, []);
+    
+      const handleLogout = async () => {
+        try{
+            const resposta = await axios.post("http://localhost:5000/logout", {}, {withCredentials: true});
+            
+            alert(resposta.data.message);
+        }
+    
+        catch(error){
+            alert("Erro ao fazer logout");
+        }
+        navigate('/');
+      }
+    
+      const showMenu = () => {
+        return (
+            <div>
+                <h1>Oi</h1>
+            </div>
+        )
+      }
+
     return (
         <header>
             <div className="nav-div">
@@ -11,8 +93,25 @@ function Header({props}) {
                         <div className="input-wrapper">
                             <input className="input-pesquisa" placeholder="Buscar produtos e marcas" />
                             <img className="icone-lupa" src="./images/lupa.png" alt="Buscar" />
+                            <div className="nome-usuario">
+                            </div>
                         </div>
+                             
                     )}
+                    {props && (
+                        <div className="container-usuario"><p onClick={toggleMenu}> {nome}</p>
+                        {menuAberto && (
+                            <div className="dropdown-menu">
+                                <ul>
+                                    <li>Perfil</li>
+                                    <li>Meus Pedidos</li>
+                                    <li>Configurações</li>
+                                    <li onClick={handleLogout}>Sair</li>
+                                </ul>
+                            </div>
+                        )}
+                            </div>
+                    )}    
                 </div>
             </div>
         </header>
